4c16d2f3b77984627966d0e758ee58069ce7b01f
micuch11 <micuch1126@gmail.com>
2024-03-04 10:17:04 +0000

    Class Method Assignment

---
+++ Class Method Assignment/Class Method Assignment/MathMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Class_Method_Assignment
+{
+    public class MathMethods
+    {
+        //Create three methods that take one integer parameter and return an integer
+        public static int Heal(int magic)
+        {
+            int hitpoints = magic + 10;
+            return hitpoints;
+        }
+
+        public static int Defense(int enemyDefense)
+        {
+            int enemy_str = 7;
+            int weapon_mt = 11;
+            int trangle_bonus = -1;
+            int crit_coeff = 1;
+            int damage = (enemy_str + (weapon_mt + trangle_bonus) * crit_coeff) - enemyDefense;
+            return damage;
+        }
+
+        public static int Attack(int strength)
+        {
+            int enemy_def = 4;
+            int weapon_mt = 8;
+            int triangle_bonus = 1;
+            int crit_coeff = 3;
+            int damage = (strength + (weapon_mt + triangle_bonus) - enemy_def) * crit_coeff;
+            return damage;
+        }
+    }
+}
---
+++ Class Method Assignment/Class Method Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Class_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method, instantiate the MathMethods class
+            MathMethods math = new MathMethods();
+            //Call the static methods from MathMethods class
+            Console.WriteLine("Input a number:");
+            int magic = Convert.ToInt32(Console.ReadLine());
+            int hitpoints = MathMethods.Heal(magic);
+            Console.WriteLine("Hitpoints after healing: " + hitpoints);
+            Console.ReadLine();
+
+            Console.WriteLine("Input enemy defense:");
+            int enemyDefense = Convert.ToInt32(Console.ReadLine());
+            int damage = MathMethods.Defense(enemyDefense);
+            Console.WriteLine("Damage dealt: " + damage);
+            Console.ReadLine();
+
+            Console.WriteLine("Input enemy strength:");
+            int strength = Convert.ToInt32(Console.ReadLine());
+            int attackDamage = MathMethods.Attack(strength);
+            Console.WriteLine("Damage dealt with attack: " + attackDamage);
+            Console.ReadLine();
+        }
+    }
+}
